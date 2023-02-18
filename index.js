955ac24602c93539dbb1dd52be7c607aa0e09caf
ses <ses@e-fork.de>
2023-02-18 16:32:16 +0100

    added router to index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const db = require("./db");
+const pageRouter = require("./routes/page-router");
 
 const app = express();
 const apiPort = 3015;
@@ -16,5 +17,7 @@ db.on("error", console.error.bind(console, "MongoDB connection error:"));
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.use("/api", pageRouter);
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));
